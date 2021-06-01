cc7bb8fab1bb833d5471f1f77ac051d1c8326428
Sumanth300 <84657589+Sumanth300@users.noreply.github.com>
2021-06-01 22:21:09 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -1,16 +1,17 @@
 import random
 
 def wallis():
-	a=1000000
+	a=10000000
 	product = 1
 	n=1
 	while (n<a):
 		product = (product*4*n*n)/(4*n*n -1)
-		n=n+1 		
+		n=n+1 	
+	print (product*2)
 	return (product*2)	
 	
 def monte_carlo():
-	a=1000000
+	a=10000000
 	square = 0
 	circle = 0
 	n=0
@@ -22,6 +23,7 @@ def monte_carlo():
 			circle += 1
 		square += 1
 	pie = 4*(circle/square)
+	print (pie)
 	return (pie)
 wallis()
 monte_carlo()
