44271f803e07ab9a1ead06f5a93d6078c1905b25
Sumanth300 <84657589+Sumanth300@users.noreply.github.com>
2021-06-01 22:10:45 +0530

    Add files via upload

---
+++ estimate.py
@@ -0,0 +1,25 @@
+import random
+
+def wallis():
+	product = 1
+	n=1
+	while (n<a):
+		product = (product*4*n*n)/(4*n*n -1)
+		n=n+1 		
+	return (product*2)	
+	
+def monte_carlo():
+	square = 0
+	circle = 0
+	n=0
+	for n in range(a):
+		x=random.random()
+		y=random.random()
+		distance = (x*x)+(y*y)
+		if (distance < 1):
+			circle += 1
+		square += 1
+	pie = 4*(circle/square)
+	return (pie)
+wallis()
+monte_carlo()
