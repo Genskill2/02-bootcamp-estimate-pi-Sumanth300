92cf837fad82353c074074aa0ae471341e916fb0
Sumanth300 <84657589+Sumanth300@users.noreply.github.com>
2021-06-01 22:00:30 +0530

    Add files via upload

--- estimate.py
+++ estimate.py
@@ -1,32 +1,29 @@
-import math
-import unittest
+import random
 
-class TestWallis(unittest.TestCase):
-    def test_low_iters(self):
-        for i in range(0, 5):
-            pi = wallis(i)
-            self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
-            
-    def test_high_iters(self):
-        for i in range(500, 600):
-            pi = wallis(i)
-            self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-
-
-class TestMC(unittest.TestCase):
-    def test_randomness(self):
-        pi0 = monte_carlo(15000)
-        pi1 = monte_carlo(15000)
-        
-        self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
-
-        self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
-
-    def test_accuracy(self):
-        for i in range(500, 600):
-            pi = monte_carlo(i)
-            self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-        
-    
-if __name__ == "__main__":
-    unittest.main()
+def wallis(b):
+	product = 1
+	a = int(b)
+	n=1
+	while (n<a):
+		product = (product*4*n*n)/(4*n*n -1)
+		n=n+1 		
+	return (product*2)	
+	
+def monte_carlo(b):
+	a = int(b)
+	square = 0
+	circle = 0
+	n=0
+	for n in range(a):
+		x=random.random()
+		y=random.random()
+		distance = (x*x)+(y*y)
+		if (distance < 1):
+			circle += 1
+		square += 1
+	pie = 4*(circle/square)
+	return (pie)
+	
+b = input("enter number of iterations ")  
+wallis(b)
+monte_carlo(b)
